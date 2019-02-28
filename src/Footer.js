fa7ab925e8c9ffbfcbfe4fa51125151090c5a652
Morgan <lockhart.mb@gmail.com>
2019-02-27 23:15:43 -0500

    inital commit with some basic header and footer styles done

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                <p>Brought to you by Morgan Lockhart, HackerYou 2019.</p>
+            </footer>
+        );
+    };
+};
+
+export default Footer;
