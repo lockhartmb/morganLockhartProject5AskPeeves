51b3030dcf965b3a48dafc95e36097f3e84a1a40
Morgan <lockhart.mb@gmail.com>
2019-03-03 12:41:58 -0500

    Updates

---
+++ precache-manifest.fb3bc1bdc15ac6ae5fcf6c484c59cb3e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c11e3434b0035037f92",
+    "url": "/project5/static/css/main.67c6ec91.chunk.css"
+  },
+  {
+    "revision": "4c11e3434b0035037f92",
+    "url": "/project5/static/js/main.4c11e343.chunk.js"
+  },
+  {
+    "revision": "6d4d9587fba49d66dfd5",
+    "url": "/project5/static/js/runtime~main.6d4d9587.js"
+  },
+  {
+    "revision": "305f35c2eaf2b7cf225b",
+    "url": "/project5/static/js/2.305f35c2.chunk.js"
+  },
+  {
+    "revision": "b2bf835607e876e4caa4610d078cfb29",
+    "url": "/project5/static/media/potterGlasses.b2bf8356.jpg"
+  },
+  {
+    "revision": "e3d55104d9a9f186da3df4f730b77f4d",
+    "url": "/project5/static/media/greyBackground.e3d55104.png"
+  },
+  {
+    "revision": "a4eef9d4bc9d12861b116f8ae4073ba6",
+    "url": "/project5/static/media/magicWand.a4eef9d4.jpg"
+  },
+  {
+    "revision": "e79b9e86f09b476bf57a77703f56847c",
+    "url": "/project5/index.html"
+  }
+];
